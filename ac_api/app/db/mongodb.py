5d500a5f42858150bf27417f796464fd9c59fdac
wijoayal <wijoayal@espol.edu.ec>
2022-08-14 14:44:52 -0500

    Add files via upload
    > heroku container:push web --app espol-smart-ac-control  , heroku container:release web --app espol-smart-ac-control, heroku login, heroku docker login

--- ac_api/app/db/mongodb.py
+++ ac_api/app/db/mongodb.py
@@ -7,4 +7,5 @@ client = MongoClient(mongo_details)
 
 db_datalake = client.datalake
 collection_stats = db_datalake.stats
-
+collection_history = db_datalake.history
+collection_history.delete_many({})
--- ac_api/app/main.py
+++ ac_api/app/main.py
@@ -3,6 +3,7 @@ from fastapi_utils.tasks import repeat_every
 from app.db import mongodb
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
+import random
 
 
 
@@ -46,16 +47,31 @@ async def ac_stats(ac_id):
     return {'doc':doc}
 
 
-@app.post("/ac_stats/{ac_id}")
-async def ac_stats(payload, ac_id):
+@app.on_event("startup")
+@repeat_every(seconds=5)
+async def time_data():
+    now = datetime.now()
+    temp = random.randint(32,34)
+    hum = random.randint(73,75)
+    ac_id = 'ac_'+str(random.randint(1,10))
+    rand = random.randint(0, 1)
+    is_on, turnoff = True, False
+    if rand == 0: 
+        is_on = True
+        turnoff = False
+    else:
+        is_on = False
+        turnoff = True
 
-    mongodb.collection_stats.update_one(
-        {'id': ac_id},
-        {'$set': {
-            'date': datetime.now(),
-            'temp': payload.split(',')[0].strip(),
-            'hum': payload.split(',')[1].strip(),
-        }}, upsert=True
+    inserted = mongodb.collection_history.insert_one(
+        {
+        'id': 'ac_1',
+        'timestamp': now,
+        'temp': temp,
+        'hum': hum,
+        'is_on': is_on,
+        'turn_off': turnoff,
+        }
     )
-    task = 'ac updated'
-    return {ac_id: task}
+    print('done')
+    print(inserted.inserted_id)
