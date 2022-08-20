c0ead05829a4138b9d5be1427992a80a2be8dfd2
wijoayal <wijoayal@espol.edu.ec>
2022-08-20 13:50:20 -0500

    final version

--- ac_api/app/main.py
+++ ac_api/app/main.py
@@ -41,37 +41,50 @@ async def ac_stats(payload, ac_id):
 
 @app.get("/ac_stats/{ac_id}")
 async def ac_stats(ac_id):
-    ac_stat = mongodb.collection_stats.find({'id': ac_id})
-    for item in ac_stat: doc = item
 
-    return {'doc':doc}
+    ac_stat = mongodb.collection_stats.find_one({'id': ac_id})
+    ac_stat.pop('_id', None)
+    ac_stat.pop('date', None)
+    return ac_stat
 
 
-@app.on_event("startup")
-@repeat_every(seconds=5)
-async def time_data():
+@app.post("/ac_stats/{ac_id}")
+async def ac_stats(payload, ac_id):
     now = datetime.now()
-    temp = random.randint(32,34)
-    hum = random.randint(73,75)
-    ac_id = 'ac_'+str(random.randint(1,10))
-    rand = random.randint(0, 1)
-    is_on, turnoff = True, False
-    if rand == 0: 
-        is_on = True
-        turnoff = False
-    else:
-        is_on = False
-        turnoff = True
-
+    mongodb.collection_stats.update_one(
+        {'id': ac_id},
+        {'$set': {
+            'date': now,
+            'temp': payload.split(',')[0].strip(),
+            'hum': payload.split(',')[1].strip(),
+        }}, upsert=True
+    )
+    task = 'ac updated'    
     inserted = mongodb.collection_history.insert_one(
         {
-        'id': 'ac_1',
-        'timestamp': now,
-        'temp': temp,
-        'hum': hum,
-        'is_on': is_on,
-        'turn_off': turnoff,
+            'id': 'ac_1',
+            'temp': payload.split(',')[0].strip(),
+            'hum': payload.split(',')[1].strip(),
+            'timestamp': now,
         }
     )
     print('done')
     print(inserted.inserted_id)
+    return {ac_id: task}
+# @app.on_event("startup")
+# @repeat_every(seconds=10)
+# async def time_data():
+#     now = datetime.now()
+#     temp = random.randint(32,34)
+#     hum = random.randint(73,75)
+#     ac_id = 'ac_'+str(random.randint(1,10))
+#     rand = random.randint(0, 1)
+#     is_on, turnoff = True, False
+#     if rand == 0: 
+#         is_on = True
+#         turnoff = False
+#     else:
+#         is_on = False
+#         turnoff = True
+
+
